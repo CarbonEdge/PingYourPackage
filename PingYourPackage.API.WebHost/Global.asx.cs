6a42def4a2d03e2ac4e3b36c7876096dd8452503
chong ching <wholetree65@gmail.com>
2017-10-09 21:37:58 +0200

    ch5

---
+++ PingYourPackage.API.WebHost/Global.asx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PingYourPackage.API.WebHost
+{
+    protected void Application_Start(object sender, EventArgs e)
+    {
+        var config = GlobalConfiguration.Configuration;
+        RouteConfig.RegisterRoutes(config);
+        WebAPIConfig.Configure(config);
+        AutofacWebAPI.Initialize(config);
+    }
+}
